0dda9e2a570c63bd4ef9d936c361addfb7802e31
Marcel Herd <me@marcelherd.com>
2016-05-13 20:09:28 +0200

    Added sample usage of persistence module

--- Source/oot/gui/src/main/java/com/marcelherd/oot/gui/App.java
+++ Source/oot/gui/src/main/java/com/marcelherd/oot/gui/App.java
@@ -1,8 +1,11 @@
 package com.marcelherd.oot.gui;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.marcelherd.oot.persistence.entity.Question;
+import com.marcelherd.oot.persistence.entity.Question.Difficulty;
 import com.marcelherd.oot.persistence.repository.QuestionService;
 import com.marcelherd.oot.persistence.repository.QuestionServiceFactory;
 
@@ -12,12 +15,47 @@ import com.marcelherd.oot.persistence.repository.QuestionServiceFactory;
  */
 public class App 
 {
+	
+	public static GameDifficulty difficulty = GameDifficulty.EASY;
+	
     public static void main( String[] args )
     {
         QuestionService questionService = QuestionServiceFactory.getInstance();
-        List<Question> questions = questionService.findAll();
-        for (Question question : questions) {
-        	System.out.println("Die Frage lautet: " + question);
+        
+        List<Question> questions = questionService.findAll(); // Gesamter Fragen-Pool
+
+        List<Question> easyQuestions = questionService.findByDifficulty(Difficulty.EASY); // einfache Fragen
+        List<Question> mediumQuestions = questionService.findByDifficulty(Difficulty.MEDIUM); // mittlere Fragen
+        List<Question> hardQuestions = questionService.findByDifficulty(Difficulty.HARD); // schwere Fragen
+        List<Question> veryHardQuestions = questionService.findByDifficulty(Difficulty.VERY_HARD); // sehr schwere Fragen
+        
+        Collections.shuffle(easyQuestions); // zufällige Fragen auswählen
+        
+        List<Question> selectedQuestions = new ArrayList<Question>(); // Fragen-Pool für die aktuelle Sitzung
+        for (int i = 0; i < difficulty.easyQuestions; i++) {
+        	selectedQuestions.add(easyQuestions.get(i));
         }
+        // analog mit den anderen Schwierigkeiten
     }
+    
+    enum GameDifficulty {
+
+    	EASY(10, 4, 1, 0),
+    	MEDIUM(6, 5, 3, 1),
+    	HARD(4, 4, 5, 2);
+
+    	final int easyQuestions;
+    	final int mediumQuestions;
+    	final int hardQuestions;
+    	final int veryHardQuestions;
+    	
+    	GameDifficulty(int easyQuestions, int mediumQuestions, int hardQuestions, int veryHardQuestions) {
+    		this.easyQuestions = easyQuestions;
+    		this.mediumQuestions = mediumQuestions;
+    		this.hardQuestions = hardQuestions;
+    		this.veryHardQuestions = veryHardQuestions;
+    	}
+    	
+    }
+    
 }
