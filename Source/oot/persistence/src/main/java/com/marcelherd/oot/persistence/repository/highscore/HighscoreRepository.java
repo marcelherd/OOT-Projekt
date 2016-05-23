a46c1ff54bb37bde3f2c89605072b68e0d67c16c
Marcel Herd <me@marcelherd.com>
2016-05-23 10:02:26 +0200

    Adjusted persistence module class uml; Fixed Anforderungsanalyse (Closes #2)

--- Source/oot/persistence/src/main/java/com/marcelherd/oot/persistence/repository/highscore/HighscoreRepository.java
+++ Source/oot/persistence/src/main/java/com/marcelherd/oot/persistence/repository/highscore/HighscoreRepository.java
@@ -5,6 +5,12 @@ import java.util.List;
 import com.marcelherd.oot.persistence.domain.Highscore;
 import com.marcelherd.oot.persistence.repository.Repository;
 
+/**
+ * This interface defines the methods to interact with the highscore
+ * domain type on the database.
+ * 
+ * @author Marcel Herd
+ */
 public interface HighscoreRepository extends Repository<Highscore, Long> {
 	
 	/**
