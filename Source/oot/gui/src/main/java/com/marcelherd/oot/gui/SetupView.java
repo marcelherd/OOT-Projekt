89fb8b16206484f699b27fb44b17ed7fb0e53765
Manuel Schwalm <manuel19972000@yahoo.de>
2016-06-21 23:23:47 +0200

    Some Corrections for maximum name length of 16 Character

--- Source/oot/gui/src/main/java/com/marcelherd/oot/gui/SetupView.java
+++ Source/oot/gui/src/main/java/com/marcelherd/oot/gui/SetupView.java
@@ -1,7 +1,11 @@
 package com.marcelherd.oot.gui;
 
+
+
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,20 +20,29 @@ import javax.swing.event.CaretListener;
 public class SetupView extends JPanel{
 	
 	
-	private JTextField nameField = new JTextField(20);
+	private JTextField nameField = new JTextField(15);
 	private JButton startButton = new JButton("Start");
 	public SetupView(GameWindow parent){
+		warning.setForeground(SystemColor.RED);
+		warning.setVisible(false);
 		startButton.setEnabled(false);
 		setLayout(new GridBagLayout());
-		JPanel panel = new JPanel();
-		panel.add(new JLabel("Your name: "));
-		panel.add(nameField);
+		
+		JPanel fieldPanel = new JPanel();
+		fieldPanel.add(new JLabel("Your name: "));
+		fieldPanel.add(nameField);
+		
+		JPanel panel = new JPanel(new GridLayout(2,2,10,10));
+		panel.add(fieldPanel);
 		panel.add(startButton);
-		setBackground(SystemColor.white);
-		panel.setBackground(SystemColor.white);
+		panel.add(warning);
+		
 		add(panel, new GridBagConstraints());
-
-		nameField.requestFocusInWindow();
+		
+		panel.setBackground(SystemColor.white);
+		setBackground(SystemColor.white);
+		fieldPanel.setBackground(SystemColor.white);
+		
 		startButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 					parent.setView(new PlayView(parent, nameField.getText()));
@@ -38,10 +51,15 @@ public class SetupView extends JPanel{
 		
 		nameField.addCaretListener(new CaretListener() {
 			public void caretUpdate(CaretEvent e) {
-					if(!nameField.getText().equals(""))
-						startButton.setEnabled(true);
-					else
+				
+						warning.setVisible(true);
 						startButton.setEnabled(false);
+					}else if(!nameField.getText().equals("")){
+						warning.setVisible(false);
+						startButton.setEnabled(true);
+					} else{
+						startButton.setEnabled(false);
+					}
 			}
 		});
 		
